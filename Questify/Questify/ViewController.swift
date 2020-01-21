6a6c237ab21de34e182ea0a8b96eb5719a57b293
SDKng <35976973+SDKng@users.noreply.github.com>
2020-01-21 16:41:41 -0500

    Added base project
    Made a simple single view project and added it to the repo.

---
+++ Questify/Questify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Questify
+//
+//  Created by Samuel Nicolas on 2020-01-21.
+//  Copyright © 2020 Questify Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
